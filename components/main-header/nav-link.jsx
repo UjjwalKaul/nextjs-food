8240d6a74deb6f17fff60ed35d99eb46672acf00
Ujjawal Kaul <ujjwal171@outlook.com>
2024-09-03 18:51:39 +0530

    Added highlighting to nav link via indiviual component

---
+++ components/main-header/nav-link.jsx
@@ -0,0 +1,20 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import classes from './nav-link.module.css';
+const NavLink = ({ href, children }) => {
+  const path = usePathname();
+  return (
+    <Link
+      className={
+        path.startsWith(href)
+          ? `${classes.active} ${classes.link}`
+          : classes.link
+      }
+      href={href}>
+      {children}
+    </Link>
+  );
+};
+
+export default NavLink;

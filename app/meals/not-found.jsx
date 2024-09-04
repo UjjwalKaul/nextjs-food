39f95b242ae0007b41a9cc08dc5f0d0677e4c82f
Ujjawal Kaul <ujjwal171@outlook.com>
2024-09-04 15:52:06 +0530

    Added meal not found component

---
+++ app/meals/not-found.jsx
@@ -0,0 +1,10 @@
+const PageNotFound = () => {
+  return (
+    <main className="not-found">
+      <h1>Meal not found</h1>
+      <p>Unfortunately we could not find the requested meal</p>
+    </main>
+  );
+};
+
+export default PageNotFound;
